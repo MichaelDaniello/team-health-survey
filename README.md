d588e6d64473c9e8d40d8d604eae560c3a77fff3
michaeldaniello <ocrmike@gmail.com>
2019-07-08 01:28:27 -0400

    Added README.md and updated survey to leverage a map for totals

---
+++ README.md
@@ -0,0 +1,9 @@
+# team-health-survey
+
+## Overview [![GoDoc](https://godoc.org/github.com/michaeldaniello/team-health-survey?status.svg)](https://godoc.org/github.com/michaeldaniello/team-health-survey)
+
+## Install
+
+```
+go get github.com/michaeldaniello/team-health-survey
+```
--- main.go
+++ main.go
@@ -6,12 +6,21 @@ import (
 	"github.com/manifoldco/promptui"
 )
 
+var totals = map[string]int{
+	"Absence of Trust":            0,
+	"Fear of Conflict":            0,
+	"Lack of Commitment":          0,
+	"Avoidance of Accountability": 0,
+	"Inattention to Results":      0,
+}
+
 func main() {
 	fmt.Println("-------------------------------------------------")
 	fmt.Println("Welcome to the Nic Cage Team Survey")
 	fmt.Println("-------------------------------------------------")
 
-	label := []string{"1. Team members are passionate and unguarded in their discussion of issues",
+	label := []string{
+		"1. Team members are passionate and unguarded in their discussion of issues",
 		"2. Team members call out one another’s deficiencies or unproductive behaviors",
 		"3. Team members know what their peers are working on and how they contribute to the collective good of the team",
 		"4. Team members quickly and genuinely apologize to one another when they say or do something inappropriate or possibly damaging to the team",
@@ -20,17 +29,17 @@ func main() {
 		"7. Team meetings are compelling, not boring",
 		"8. Team members leave meetings confident that their peers are completely committed to the decisions that were agreed on, even if they were in initial disagreement",
 		"9. Morale is significantly affected by the failure to achieve team goals",
-		"10.During team meetings, the most important—and difficult—issues are put on the table to be resolved",
+		"10. During team meetings, the most important—and difficult—issues are put on the table to be resolved",
 		"11. Team members are deeply concerned about the prospect of letting down their peers",
 		"12. Team members know about one another’s personal lives and are comfortable discussing them",
 		"13. Team members end discussions with clear and specific resolutions and action plans",
 		"14. Team members challenge one another about their plans and approaches",
-		"15. Team members are slow to seek credit for their own contributions, but quick to point out those of others"}
-	total := 0
+		"15. Team members are slow to seek credit for their own contributions, but quick to point out those of others",
+	}
 
-	for i := 0; i < len(label); i++ {
+	for i := 1; i <= len(label); i++ {
 		prompt := promptui.Select{
-			Label: label[i],
+			Label: label[i-1],
 			Items: []string{"Usually", "Sometimes", "Rarely"},
 		}
 
@@ -40,18 +49,76 @@ func main() {
 			fmt.Printf("Prompt failed %v.\n", err)
 			return
 		}
-		fmt.Printf("You choose %q.\n", result)
-		switch {
-		case result == "Usually":
-			total += 3
-		case result == "Sometimes":
-			total += 2
-		default:
-			total++
-		}
-		fmt.Printf("Your total is %v.\n", total)
+		resultChecker(result, categoryGetter(i))
 	}
+	fmt.Println(
+		"Your score for Absence of Trust is",
+		totals["Absence of Trust"],
+		"\nYour score for Fear of Conflict is",
+		totals["Fear of Conflict"],
+		"\nYour score for Lack of Commitment is",
+		totals["Lack of Commitment"],
+		"\nYour score for Avoidance of Accountability is",
+		totals["Avoidance of Accountability"],
+		"\nYour score for Inattention to Results is",
+		totals["Inattention to Results"],
+	)
+	fmt.Println(
+		"-------------------------------------------------",
+		"A score of 3 to 5 is probably an indication that the dysfunction needs to be addressed.",
+		"A score of 6 or 7 indicates that the dysfunction could be a problem.",
+		"A score of 8 or 9 is a probable indication that the dysfunction is not a problem for your team.",
+		"-------------------------------------------------\n",
+		`Reguardless of your scores, it is important to keep in mind that every team needs constant work,
+	 	because without it, even the best ones deviate toward dysfunction.`)
+}
 
-	// fmtPrintf("You choose %q\n", result)
+func resultChecker(result string, category string) {
+
+	switch {
+	case result == "Usually":
+		totals[category] += 3
+	case result == "Sometimes":
+		totals[category] += 2
+	default:
+		totals[category]++
+	}
+}
+
+func categoryGetter(questionNum int) string {
+	fmt.Println(questionNum)
+	switch {
+	case questionNum == 4:
+		return "Absence of Trust"
+	case questionNum == 6:
+		return "Absence of Trust"
+	case questionNum == 12:
+		return "Absence of Trust"
+	case questionNum == 1:
+		return "Fear of Conflict"
+	case questionNum == 7:
+		return "Fear of Conflict"
+	case questionNum == 10:
+		return "Fear of Conflict"
+	case questionNum == 3:
+		return "Lack of Commitment"
+	case questionNum == 8:
+		return "Lack of Commitment"
+	case questionNum == 13:
+		return "Lack of Commitment"
+	case questionNum == 2:
+		return "Avoidance of Accountability"
+	case questionNum == 11:
+		return "Avoidance of Accountability"
+	case questionNum == 14:
+		return "Avoidance of Accountability"
+	case questionNum == 5:
+		return "Inattention to Results"
+	case questionNum == 9:
+		return "Inattention to Results"
+	case questionNum == 15:
+		return "Inattention to Results"
+	}
+	return ""
 
 }
